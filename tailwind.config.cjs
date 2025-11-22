1b9e13f4b7188738fdfe99fd630b48f5cc2c522e
cityfish91159 <cityfish91159@gmail.com>
2025-11-22 09:55:31 +0000

    Style: Increase mascot float animation height by 2px

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -51,7 +51,7 @@ module.exports = {
         },
         float: {
           '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-5px)' },
+          '50%': { transform: 'translateY(-7px)' },
         }
       },
     }
